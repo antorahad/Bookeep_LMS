7c3aa68d05aec1208a9f83aef8c57da288bfb479
antorahad <antorahad6@gmail.com>
2024-03-29 00:11:24 +0600

    firebase variable hidden

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyA8t3s2rG2csSElaP5ot10oPyYVseSyzhY",
-  authDomain: "bookeep-lms.firebaseapp.com",
-  projectId: "bookeep-lms",
-  storageBucket: "bookeep-lms.appspot.com",
-  messagingSenderId: "585810721507",
-  appId: "1:585810721507:web:de3b3c7e3a1ebe89b5a3d6"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
 };
 
 const app = initializeApp(firebaseConfig);
